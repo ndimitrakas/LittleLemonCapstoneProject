1d740d37017e2525d1e1a30261a33ffa25fb7108
nikolakis321 <ndimitrakas1@pride.hofstra.edu>
2024-05-23 15:53:03 -0400

    Created chicago component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import Main from './components/Main';
+import Chicago from './components/Chicago';
 import Footer from './components/Footer';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Header />
       <Hero />
       <Main />
+      <Chicago />
       <Footer />
     </>
   );
---
+++ src/components/Chicago.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function Chicago() {
+  return (
+    <section className="chicago">
+      <h2>About Little Lemon Chicago</h2>
+      <p>Little Lemon is a family-owned Mediterranean restaurant located in the heart of Chicago. We are known for our delicious lemon-themed dishes and welcoming atmosphere.</p>
+    </section>
+  );
+}
+
+export default Chicago;
