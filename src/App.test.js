18cf26bfacb76e60313554cbeeb70827333d3b68
nikolakis321 <ndimitrakas1@pride.hofstra.edu>
2024-05-25 13:33:00 -0400

    Fixed problem with app.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -2,10 +2,9 @@ import React, { act } from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-// Skip the test
-test.skip('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test('renders Main component in App', () => {
+  const { getByTestId } = render(<App />); // Render your App component
+  const mainComponent = getByTestId('main-component'); // Assuming your Main component has a test ID
 
+  expect(mainComponent).toBeInTheDocument(); // Check if the Main component is in the document
+});
